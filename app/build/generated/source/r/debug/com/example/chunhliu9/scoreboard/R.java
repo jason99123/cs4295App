cc01170eeb5b03aa53fa0f7292974b63e566f304
Noob <Noob>
2016-11-18 10:34:42 +0800

    comment

--- app/build/generated/source/r/debug/com/example/chunhliu9/scoreboard/R.java
+++ app/build/generated/source/r/debug/com/example/chunhliu9/scoreboard/R.java
@@ -11,8 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
@@ -31,13 +31,13 @@ public final class R {
         public static final int menu_main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060000;
+        public static final int app_name=0x7f060001;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /**  Customize your theme here. 
          */
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f040000;
     }
 }
